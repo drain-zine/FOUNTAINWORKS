bafa2b5964cb840164e48593fd74dd243211e0e5
Tom Stannett <invisibletree@protonmail.ch>
2021-09-17 12:54:40 +0100

    Initial commit

---
+++ client/src/App.js
@@ -0,0 +1,53 @@
+import React, {useEffect, useState} from "react";
+import logo from './logo.svg';
+import './App.css';
+
+const App = () => {
+
+  const [connectionStatus, setConnectionStatus] = useState({});
+
+  const fetchConnection = async () => {
+    const response = await fetch('/express_backend');
+    const body = await response.json();
+
+    if (response.status !== 200) {
+      throw Error(body.message) 
+    }
+    return body;
+  };
+
+  useEffect(() => {
+    const connectToBackEnd = async() => {
+      try{
+        const resp = await fetchConnection();
+        setConnectionStatus({data: resp.express});
+      }catch(e){
+        console.log(e);
+      }
+    };
+
+    connectToBackEnd();
+
+  }, []);
+
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
+    </div>
+  );
+}
+
+export default App;
---
+++ server/server.js
@@ -0,0 +1,13 @@
+const express = require("express");
+const app = express();
+const port = process.env.PORT || 5000;
+
+app.listen(port, () => {
+    console.log(`[INFO] Listening on port ${port}...`);
+});
+
+
+// ROUTES
+app.get('/express_backend', (req, res) => {
+    res.send({ express: 'Connected to Express' });
+  });
