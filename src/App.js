831d7650ca85c45957c2cdc29e2b7cc234454eee
Tom Stannett <invisibletree@protonmail.ch>
2021-10-02 18:59:10 +0100

    Must integrate webpack stuff for server

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React from 'react';
-import Home from "./pages/Home";
+import Landing from "./pages/Landing/Landing";
 
 export default () => {
-  return <Home name="Sammy" />;
+  return <Landing />;
 };
---
+++ src/pages/Landing/Landing.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import styles from './Landing.module.scss';
+
+const Landing = () => {
+  return (
+    <main className={styles.page}>
+      <section className={styles.titleBar}>
+        <p>"FOUNTAIN WORKS"</p>
+      </section>
+    </main>
+  );
+};
+
+export default Landing;
